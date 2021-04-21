0aa8a866829e6b8147ae5dec601b5fb95514e066
longphamit <longphamjustdoit@gmail.com>
2021-04-21 11:35:26 +0700

    update package

---
+++ src/main/java/com/fptu/latoshopspringboot/app/mapper/ModelMapperCustom.java
@@ -0,0 +1,8 @@
+package com.fptu.latoshopspringboot.app.mapper;
+
+import org.modelmapper.ModelMapper;
+
+//This class convert DTO and Entity
+public class ModelMapperCustom {
+    private static ModelMapper MODEL_MAPPER= new ModelMapper();
+}
