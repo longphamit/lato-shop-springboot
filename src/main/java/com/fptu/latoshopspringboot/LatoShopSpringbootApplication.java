eadb06ef3320408d6b4462a5523d8864584b006c
longphamit <longphamjustdoit@gmail.com>
2021-04-21 11:16:39 +0700

    init project

---
+++ src/main/java/com/fptu/latoshopspringboot/LatoShopSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.fptu.latoshopspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatoShopSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatoShopSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/fptu/latoshopspringboot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.fptu.latoshopspringboot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LatoShopSpringbootApplication.class);
+    }
+
+}
